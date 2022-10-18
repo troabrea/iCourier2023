3324e0c64074afbca75a3d62f9cb881bb742c621
temis <temis@barolit.com>
2022-10-18 06:49:06 -0400

    CodeMagic YAML

--- tools/build_android_bundle.sh
+++ tools/build_android_bundle.sh
@@ -1,1 +1,3 @@
-flutter build appbundle -t lib/main_caribepack.dart --flavor caribepack
+#flutter build appbundle -t lib/main_caribepack.dart --flavor caribepack
+
+flutter build apk -t lib/main_bmcargo.dart --flavor bmcargo --split-per-abi
